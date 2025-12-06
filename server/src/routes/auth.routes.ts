7c379662c0ad41ea4a4a226a597c1fe6296d0188
bahiya666 <126662308+bahiya666@users.noreply.github.com>
2025-12-06 15:38:29 +0200

    Increased validation

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, deleteAccount, me } from "../controllers/auth.controller.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+// protected routes
+router.get("/me", protect, me);
+router.delete("/me", protect, deleteAccount);
 
 export default router;
