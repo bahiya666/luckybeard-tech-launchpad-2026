ffbb6e4c4334317c9cd8d048b7517bca894ed838
bahiya666 <126662308+bahiya666@users.noreply.github.com>
2025-12-08 04:39:33 +0200

    Application dockerised

--- server/wait-for-db.sh
+++ server/wait-for-db.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-# wait-for-db.sh
-
 set -e
 
 host="$1"
 shift
-cmd="$@"
 
-until pg_isready -h "$host" -U "postgres"; do
-  echo "Waiting for Postgres at $host..."
+until pg_isready -h "$host" -p 5432 -U "postgres"; do
+  echo "Waiting for database at $host:5432..."
   sleep 2
 done
 
-exec $cmd
+exec "$@"
